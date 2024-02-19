e83bbb9c855b932803439f1f4fdef1388c2fb2e3
mushfiq7297 <mushfiqurrahman7297@gmail.com>
2024-02-19 22:10:55 +0600

    final project done

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -12,50 +12,64 @@ button.disabled = true;
 
 for(const btn of allBtn){
     // console.log(btn);
-    btn.addEventListener("click", function (e){
-        count+=1;
-        if(count > 4){
-            return;
-         }
-        btn.style.color = 'white';
-        btn.style.backgroundColor = '#1DD100';
-        
-        setInerText("seat-count",count)
-        setInerText("seat-left", 40-count)
-        
-
-         const seatName = e.target.childNodes[0].textContent;
-         const selectedContainer = document.getElementById("selected-container");
-         const addedSeatInfo = document.getElementById("add-line");
-         const li = document.createElement("li");
-         const h1 = document.createElement("h1");
-         h1.innerText = seatName
-         const h2 = document.createElement("h2");
-         h2.innerText = "Economy";
-         const h3 = document.createElement("h3");
-         h3.innerText = "550"
-         
-         li.appendChild(h1);
-         li.appendChild(h2);
-         li.appendChild(h3);
-         selectedContainer.appendChild(li);
-         setInerText("total-price",count*550);
-         setInerText("grand-total", count*550);
-          
-        const lengthNumber = document.getElementById("length-number").value;
-        const lengthOfNumber = parseInt(lengthNumber)
-        console.log(lengthOfNumber);
-
-         if(count>0 && lengthNumber>0)
-         {
-            const button = document.querySelector('#disabled-btn');
-            button.disabled = false;
-         }
-         
- });
-}
-
+    
+            btn.addEventListener("click", function (e){
+            count+=1;
+            if(count > 4){
+                return;
+             }
+            btn.style.color = 'white';
+            btn.style.backgroundColor = '#1DD100';
+            setInerText("seat-count",count)
+            setInerText("seat-left", 40-count)
+            if(btn.style.backgroundColor == '#1DD100')
+            {
+                btn.disabled = true;
+                return;
+            }
+    
+             const seatName = e.target.childNodes[0].textContent;
+             const selectedContainer = document.getElementById("selected-container");
+             const addedSeatInfo = document.getElementById("add-line");
+             const li = document.createElement("li");
+             const h1 = document.createElement("h1");
+             h1.innerText = seatName
+             const h2 = document.createElement("h2");
+             h2.innerText = "Economy";
+             const h3 = document.createElement("h3");
+             h3.innerText = "550"
+             
+             li.appendChild(h1);
+             li.appendChild(h2);
+             li.appendChild(h3);
+             selectedContainer.appendChild(li);
+             setInerText("total-price",count*550);
+             setInerText("grand-total", count*550);
+             
+            const lengthNumber = document.getElementById("length-number").value.length;
+            const lengthOfNumber = parseInt(lengthNumber);
+            console.log(lengthOfNumber);
+            console.log(count);
+            
+            if(lengthOfNumber>0 && count>0)
+             {
+                const button = document.querySelector('#disabled-btn');
+                button.disabled = false;
+             }
+             
+            });
+        }
 function next(){
     hideElementById("main-section");
     showElementById("success-page");
 }
+
+const lengthNumber = document.getElementById("length-number").value.length;
+            const lengthOfNumber = parseInt(lengthNumber);
+document.getElementById("length-number").addEventListener("input", function() {
+    if(lengthOfNumber>0 && count>0)
+ {
+    const button = document.querySelector('#disabled-btn');
+    button.disabled = false;
+ }
+})
